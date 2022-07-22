aba1641f047c330240322073fa3520b78845effd
roberte777 <robewilkes@yahoo.com>
2022-07-22 16:43:33 -0500

    init

--- client/main.go
+++ client/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strings"
 	"time"
 )
 
 //make a client that connects to tcp server
 func main() {
-	const CONNECTION = "172.16.0.6:80"
+	const CONNECTION = "localhost:3000"
 	//connect to server
 	c, err := net.Dial("tcp", CONNECTION)
 	if err != nil {
@@ -27,6 +29,7 @@ func main() {
 		time.Sleep(time.Second * 1)
 	}
 }
+
 func read_data(c net.Conn) {
 	//make a buffer to read data into
 	buf := make([]byte, 5)
@@ -50,8 +53,23 @@ func write_data(c net.Conn) {
 		var input string
 		fmt.Scanln(&input)
 
-		//send user input to server
-		c.Write([]byte(input))
+		buffer := make([]byte, 5)
+		//new string stream
+		stream := strings.NewReader(input)
+		for {
+			//read a byte from the stream
+			_, err := stream.Read(buffer)
+			if err == io.EOF {
+				fmt.Println("End of file end of here: " + string(buffer))
+				end := make([]byte, 1)
+				end[0] = 0
+				c.Write(end)
+				break
+			}
+			//write the byte to the connection
+			c.Write(buffer)
+		}
+		fmt.Println("Sent: " + input)
 
 	}
 }
--- server/main.go
+++ server/main.go
@@ -9,8 +9,8 @@ import (
 //make a tcp server that listens on port 8080
 func main() {
 	const (
-		PORT = "80"
-		HOST = "172.16.0.6"
+		PORT = "3000"
+		HOST = "localhost"
 		TYPE = "tcp"
 	)
 	//listen on port 8080
@@ -37,14 +37,25 @@ func handleConnection(c net.Conn) {
 	fmt.Println("Handling connection")
 	buffer := make([]byte, 5)
 	for {
-		n, err := c.Read(buffer)
+		message := ""
+		for {
+			n, err := c.Read(buffer)
 
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			if buffer[0] == 0 {
+				break
+			}
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			//print message to console
+			fmt.Println(string(buffer[:n]))
+			message += string(buffer[:n])
 		}
-
-		c.Write([]byte(string(buffer[:n])))
+		fmt.Println("Message: " + message)
+		//write finished
+		c.Write([]byte("Finished"))
 
 	}
 }
